b05eff48f3d03989f74617cf0620fe9513466bdb
theakhilsarkar18 <akhilsodvadiya7@gmail.com>
2023-04-24 12:05:14 +0530

    24-04-2023

--- lib/home/homescreeen.dart
+++ lib/home/homescreeen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class Homescreen extends StatefulWidget {
   const Homescreen({Key? key}) : super(key: key);
@@ -10,16 +11,27 @@ class Homescreen extends StatefulWidget {
 class _HomescreenState extends State<Homescreen> {
   @override
   Widget build(BuildContext context) {
+
+
+    TextEditingController txtName = TextEditingController();
+    TextEditingController txtPass = TextEditingController();
+
     return SafeArea(
       child: Scaffold(
         body: Column(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
+            TextField(controller: txtName,),
+            TextField(controller: txtPass,),
+            ElevatedButton(onPressed: () {
+              addData();
+            }, child: Text("Login"),),
+
             TextField(),
             TextField(),
             ElevatedButton(onPressed: () {
 
-            }, child: Text("Login"),),
+            }, child: Text("SignIn"),),
           ],
         ),
       ),
@@ -28,5 +40,23 @@ class _HomescreenState extends State<Homescreen> {
 }
 
 
+String? name,password;
 
+Future<void> addData()
+async {
+  SharedPreferences s1 =  await SharedPreferences.getInstance();
+  s1.setString('name', name!);
+  s1.setString('pass', password!);
+}
 
+Future<Map> getData()
+async {
+  SharedPreferences s1 =  await SharedPreferences.getInstance();
+  String? name = s1.getString('name');
+  String? pass = s1.getString('pass');
+  Map m1 = {
+    'name':name,
+    'pass':pass,
+  };
+  return m1;
+}
